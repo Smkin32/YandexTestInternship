96ce6e4474daa6b980c583056132dfce60baacf6
Smkin32 <adiksem2005@gmail.com>
2026-02-05 20:49:01 +0300

    Added simple GET, PUT, POST, DELETE tests

--- src/test/java/TestAPI.java
+++ src/test/java/TestAPI.java
@@ -7,6 +7,14 @@ import java.net.http.HttpResponse;
 public class TestAPI {
     static String apiHost;
     static String oauth;
+
+    static String testCopyDirectoryName = "testCopy";
+    static String testCreateDirectoryName = "test";
+    static String testDeleteDirectoryName = "testDelete";
+
+    static boolean testCopyNeedCleanup = false;
+    static boolean testCreateNeedCleanup = false;
+
     String resource;
     String method;
     String[] parameters;
@@ -18,6 +26,15 @@ public class TestAPI {
         oauth = dotenv.get("OAUTH_TOKEN");
         APIHandler.setHost(apiHost);
         APIHandler.setAuth(oauth);
+
+        setupTests();
+    }
+
+    static void setupTests(){
+        createSampleDirectory(testCopyDirectoryName);
+        testCopyNeedCleanup = true;
+
+        createSampleDirectory(testDeleteDirectoryName);
     }
 
     @Test
@@ -36,5 +53,102 @@ public class TestAPI {
         }
 
         Assertions.assertEquals(200, rsp.statusCode());
+    }
+
+    @Test
+    void createDirectory(){
+        resource = "/resources";
+        method = "PUT";
+        parameters = new String[]{"path="+testCreateDirectoryName};
+
+        APIHandler handler = new APIHandler(resource, method, parameters);
+
+        HttpResponse<String> rsp;
+        try {
+            rsp = handler.getResponse();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertEquals(201, rsp.statusCode());
+
+        testCreateNeedCleanup = true;
+    }
+
+    @Test
+    void copyDirectory(){
+        resource = "/resources/copy";
+        method = "POST";
+        parameters = new String[]{"from="+testCopyDirectoryName,"path="+testCopyDirectoryName+"2"};
+
+        APIHandler handler = new APIHandler(resource, method, parameters);
+
+        HttpResponse<String> rsp;
+        try {
+            rsp = handler.getResponse();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(rsp.body());
+        Assertions.assertEquals(201, rsp.statusCode());
+    }
+
+    @Test
+    void deleteDirectory(){
+        resource = "/resources";
+        method = "DELETE";
+        parameters = new String[]{"path="+testDeleteDirectoryName};
+
+        APIHandler handler = new APIHandler(resource, method, parameters);
+
+        HttpResponse<String> rsp;
+        try {
+            rsp = handler.getResponse();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertEquals(204, rsp.statusCode());
+
+    }
+
+    static void createSampleDirectory(String name){
+        APIHandler handler = new APIHandler("/resources", "PUT", new String[]{"path="+name});
+
+        HttpResponse<String> rsp;
+        try {
+            rsp = handler.getResponse();
+            if (rsp.statusCode() != 201){
+                throw new IOException();
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void deleteSampleDirectory(String name){
+        APIHandler handler = new APIHandler("/resources", "DELETE", new String[]{"path="+name});
+
+        HttpResponse<String> rsp;
+        try {
+            rsp = handler.getResponse();
+            if (rsp.statusCode() != 204){
+                throw new IOException();
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @AfterAll
+    static void cleanUpAndWait(){
+        if (testCopyNeedCleanup){
+            testCopyNeedCleanup = false;
+            deleteSampleDirectory(testCopyDirectoryName);
+            deleteSampleDirectory(testCopyDirectoryName+"2");
+        }
+        if (testCreateNeedCleanup){
+            testCreateNeedCleanup = false;
+            deleteSampleDirectory(testCreateDirectoryName);
+        }
     }
 }
